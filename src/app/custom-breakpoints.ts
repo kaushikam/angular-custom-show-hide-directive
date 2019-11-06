33a0e06463a74c806922c8a52da9525336f2f4ac
kaushikam <kaushikayanam@gmail.com>
2019-11-06 08:23:23 +0530

    1 file modified

--- src/app/custom-breakpoints.ts
+++ src/app/custom-breakpoints.ts
@@ -3,7 +3,7 @@ import {BREAKPOINT} from '@angular/flex-layout';
 const PRINT_BREAKPOINTS = [{
   alias: 'xs.print',
   suffix: 'XsPrint',
-  mediaQuery: 'print and (max-width: 297px)',
+  mediaQuery: 'print',
   overlapping: false
 }];
 
